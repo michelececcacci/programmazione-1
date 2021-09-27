0ef307122d233c49a02e60eac81ff4613a9891de
michelececcacci <michelececcacci1@gmail.com>
2021-09-27 18:59:02 +0200

    aggiunto EXTREME_POINT E MAXIMUM_POINT, testato le funzioni e corretto i commenti per compilare in ascii

--- elaborato2/elaborato2.c
+++ elaborato2/elaborato2.c
@@ -2,10 +2,12 @@
 #include "quadratic_eq.h"
 
 int main() {
-    printf("%d", NUM_OF_ROOTS(0.5,-2.0,1.5)); // 2
-    printf("%d", NUM_OF_ROOTS(0.5,-2.0,2.0)); // 1
-    printf("%d\n", NUM_OF_ROOTS(-0.5,-2.0,-3.0)); // 0
+    printf("%d ", NUM_OF_ROOTS(0.5,-2.0,1.5)); /* 2 */
+    printf("%d ", NUM_OF_ROOTS(0.5,-2.0,2.0)); /* 1 */
+    printf("%d\n", NUM_OF_ROOTS(-0.5,-2.0,-3.0));  /* 1 */
 
-    printf("%f ", ROOT1(0.5,-2.0,1.5)); // 3.0
-    printf("%f\n", ROOT2(0.5,-2.0,1.5)); // 1.0
+    printf("%f ", ROOT1(0.5,-2.0,1.5)); /* 3 */
+    printf("%f\n", ROOT2(0.5,-2.0,1.5)); /* 1.0 */
+    printf("%+f\n", EXTREME_POINT(0.5, -2.0, 1.5));  /* 2.0 */
+    printf("%i\n", MAXIMUM_POINT(0.5, -2.0, 1.5));  /* 0 */
 }
--- elaborato2/quadratic_eq.h
+++ elaborato2/quadratic_eq.h
@@ -5,8 +5,13 @@
 /* La macro ROOT ottiene il risultato dell'equazione di secondo grado ((-b +- sqrt(delta)) / 2a)
  * da a, b, c e "sign", che può essere 1 per il risultato maggiore o -1 per quello minore. */
 #define ROOT(a, b, c, sign) (-b + sign * sqrt(DELTA(a, b, c))) / (2 * a)
+/* La macro f(x) calcola il valore della y della funzione in un punto x*/
+#define f(x, a, b, c) (pow(x, 2) * a + x * b + c)
 
 /* Macro della consegna */
 #define NUM_OF_ROOTS(a, b, c) ({double delta = DELTA(a, b, c); delta < 0 ? 0 : delta > 0 ? 2 : 1;})
 #define ROOT1(a, b, c) ROOT(a, b, c, 1)
 #define ROOT2(a, b, c) ROOT(a, b, c, -1)
+#define EXTREME_POINT(a, b, c) -b / (2 * a)
+/* Se anche solo un punto dell' equazione  e maggiore di una radice, allora il punto e necessariamente un minimo, se no e un massimo. */
+#define MAXIMUM_POINT(a, b, c)  f(EXTREME_POINT(a, b, c), a, b, c)>= f(ROOT1(a, b, c), a, b, c) && f(EXTREME_POINT(a, b, c), a, b, c)>= f(ROOT2(a, b, c), a, b, c) 
