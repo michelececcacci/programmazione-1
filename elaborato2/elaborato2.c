a615d47db9afa9d7e988d417d90afea65d3ac531
Giorgio Garofalo <giogar2705@gmail.com>
2021-09-27 13:27:55 +0200

    NUM_OF_ROOTS

--- elaborato2/elaborato2.c
+++ elaborato2/elaborato2.c
@@ -1,3 +1,10 @@
-int main() {
+#include <stdio.h>
+#include <math.h>
 
+#define DELTA(a, b, c) b * b - 4 * a * c;
+#define NUM_OF_ROOTS(a, b, c) ({double delta = DELTA(a, b, c); delta < 0 ? 0 : delta > 0 ? 2 : 1;})
+
+int main() {
+    printf("%d", NUM_OF_ROOTS(1, 2, 3));
+    printf("%d", NUM_OF_ROOTS(1, -2, -1));
 }
