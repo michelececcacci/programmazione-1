e4d02806bd4ac777c17cdb25371fb416e5283038
Giorgio Garofalo <giogar2705@gmail.com>
2021-09-27 13:50:31 +0200

    ROOT1 & ROOT2

--- elaborato2/elaborato2.c
+++ elaborato2/elaborato2.c
@@ -4,4 +4,7 @@
 int main() {
     printf("%d", NUM_OF_ROOTS(1, 2, 3));
     printf("%d", NUM_OF_ROOTS(1, -2, -1));
+    printf("\n");
+    printf("%f\n", ROOT1(1, -2, -1));
+    printf("%f\n", ROOT2(1, -2, -1));
 }
--- elaborato2/quadratic_eq.h
+++ elaborato2/quadratic_eq.h
@@ -1,2 +1,6 @@
-#define DELTA(a, b, c) b * b - 4 * a * c;
+#include "math.h"
+
+#define DELTA(a, b, c) b * b - 4 * a * c
 #define NUM_OF_ROOTS(a, b, c) ({double delta = DELTA(a, b, c); delta < 0 ? 0 : delta > 0 ? 2 : 1;})
+#define ROOT1(a, b, c) (-b + sqrt(DELTA(a, b, c))) / (2 * a)
+#define ROOT2(a, b, c) (-b - sqrt(DELTA(a, b, c))) / (2 * a)
