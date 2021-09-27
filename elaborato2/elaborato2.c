6cb32ab65e82d1190bacbe3f410b5b88fef3c1c7
Giorgio Garofalo <giogar2705@gmail.com>
2021-09-27 14:03:15 +0200

    Update tests

--- elaborato2/elaborato2.c
+++ elaborato2/elaborato2.c
@@ -2,9 +2,10 @@
 #include "quadratic_eq.h"
 
 int main() {
-    printf("%d", NUM_OF_ROOTS(1, 2, 3));
-    printf("%d", NUM_OF_ROOTS(1, -2, -1));
-    printf("\n");
-    printf("%f\n", ROOT1(1, -2, -1));
-    printf("%f\n", ROOT2(1, -2, -1));
+    printf("%d", NUM_OF_ROOTS(0.5,-2.0,1.5)); // 2
+    printf("%d", NUM_OF_ROOTS(0.5,-2.0,2.0)); // 1
+    printf("%d\n", NUM_OF_ROOTS(-0.5,-2.0,-3.0)); // 0
+
+    printf("%f ", ROOT1(0.5,-2.0,1.5)); // 3.0
+    printf("%f\n", ROOT2(0.5,-2.0,1.5)); // 1.0
 }
