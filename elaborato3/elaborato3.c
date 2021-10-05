c9d6ec593843b9838849639d7c7ded8fb6964e56
Giorgio Garofalo <giogar2705@gmail.com>
2021-10-05 09:54:51 +0200

    Loop separati

--- elaborato3/elaborato3.c
+++ elaborato3/elaborato3.c
@@ -6,19 +6,33 @@ int main() {
     printf("Inserire un numero: ");
     scanf("%u", &input);
 
-    int reversed = 0;
-	int size = 0;
+    /* Punto 3 */
     int zeros = 0;
+    unsigned int n = input;
+    while (n > 0) {
+        if(n % 10 == 0) zeros++;
+        n /= 10;
+    }
 
-	for (int n = input; n != 0; n /= 10) {
-        int rem = n % 10;
-        reversed = reversed * 10 + rem;
-        if(rem == 0) zeros++;
-		size++;
+    /* Punto 1 */
+    unsigned int reversed = 0;
+    for (n = input; n > 0; n /= 10) {
+        reversed = reversed * 10 + (n % 10);
 	}
 
+    /* Punto 2 */
+    int size = 0;
+    n = input;
+    do {
+        size++;
+        n /= 10;
+    } while (n > 0);
+
+    int compl = (int) (pow(10, size) - input);
+
+    /* Output */
     printf("%d", reversed);
-    printf(" %d", (int) pow(10, size) - input);
+    printf(" %d", compl);
     printf(" %d", zeros);
 
 	printf("\n");
