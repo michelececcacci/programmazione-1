92dacd3f2b0cd43319f70e63e5625842bec443d5
Michele Ceccacci <michelececcacci1@gmail.com>
2021-10-06 13:04:23 +0200

    corretto punto 3, cambiate variabili  per codeboard

--- elaborato3/elaborato3.c
+++ elaborato3/elaborato3.c
@@ -7,36 +7,36 @@ int main() {
 
     unsigned int n = x;
     /* Punto 1 */
-    unsigned int reversed = 0;
+    unsigned int res1 = 0; /* res1 = reversed */
     for (n = x; n > 0; n /= 10) {
-        reversed = reversed * 10 + (n % 10);
+        res1 = res1 * 10 + (n % 10);
 	}
 
     /* Punto 2 */
-    int size = 0;
+    int res2 = 0; /* res2 = size */
     n = x;
     do {
-        size++;
+        res2++;
         n /= 10;
     } while (n > 0);
 
     /* Punto 3 */
-    int zeros = 0;
+    int res3 = 0; /* res3 = zeros */
     n = x;
     while (n > 0) {
         if((n % 10)== 0) {
-            zeros++;
+            res3++;
         }
         n /= 10;
     }
 
 
-    int compl = (int) (pow(10, size) - x);
+    int compl = (int) (pow(10, res2) - x);
 
     /* Output */
-    printf("%d", reversed);
+    printf("%d", res1);
     printf(" %d", compl);
-    printf(" %d", zeros);
+    printf(" %d", res3);
 
 	printf("\n");
 }
