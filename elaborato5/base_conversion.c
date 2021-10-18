e5b34df8c6e2a314b0e800916d99f7b02ec0c9b8
Giorgio Garofalo <giogar2705@gmail.com>
2021-10-18 13:29:59 +0200

    Reformat

--- elaborato5/base_conversion.c
+++ elaborato5/base_conversion.c
@@ -2,50 +2,66 @@
 #include "stdio.h"
 #include <math.h>
 
+/*
+* Procedure per stampare un numero n in base b.
+* La base b deve essere compresa tra 2 e 16,
+* diversamente le procedure non stampano nulla.
+*/
+
 // works, but in least singificant from most significant
 void base_conversion_rc(unsigned int n, unsigned int b) {
-    if (n == 0) return; 
-    if (n > 0)  
-    {
+    if (n == 0) return;
+    if (n > 0) {
         base_conversion_rc(n / b, b);
         printf("%d", n % b);
     }
-
 }
 
 
 // not working, prints number backwards
 void base_conversion_it(unsigned int n, unsigned int b) {
     int digits;
-    int n1 = n;
-    for (digits = 0; n1 > 0; digits++ ){
-        n1 /= b;
+    for (digits = 0; n > 0; digits++) {
+        n /= b;
     }
     printf("number of digits in new base: %d\n", digits);
-
-    return;
-} 
+}
 
 
-char convert_to_base(int n){
+char convert_to_base(int n) {
     char ch;
-    switch (n){
-        case 0: ch = '0'; break;
-        case 1: ch = '1'; break;
-        case 2: ch = '2'; break;
-        case 3: ch = '3'; break;
-        case 4: ch = '4'; break;
-        case 5: ch = '5'; break;
-        case 6: ch = '6'; break;
-        case 7: ch = '7'; break;
-        case 8: ch = '8'; break;
-        case 9: ch = '9'; break;
-        case 10: ch = 'A'; break;
-        case 11: ch = 'B'; break;
-        case 12: ch = 'C'; break;
-        case 13: ch = 'D'; break;
-        case 14: ch = 'E'; break;
-        case 15: ch = 'F'; break;
+    switch (n) {
+        case 0:
+            return '0';
+        case 1:
+            return '1';
+        case 2:
+            return '2';
+        case 3:
+            return '3';
+        case 4:
+            return '4';
+        case 5:
+            return '5';
+        case 6:
+            return '6';
+        case 7:
+            return '7';
+        case 8:
+            return '8';
+        case 9:
+            return '9';
+        case 10:
+            return 'A';
+        case 11:
+            return 'B';
+        case 12:
+            return 'C';
+        case 13:
+            return 'D';
+        case 14:
+            return 'E';
+        case 15:
+            return 'F';
     }
-    return ch;
 }
