f76cfbe9871ee264cf87850a77d8304d33d6767d
michelececcacci <michelececcacci1@gmail.com>
2021-10-18 19:02:19 +0200

    better char convertion

--- elaborato5/base_conversion.c
+++ elaborato5/base_conversion.c
@@ -32,38 +32,7 @@ void base_conversion_it(unsigned int n, unsigned int b) {
 }
 
 static char convert_to_char(unsigned int n) {
-    switch (n) {
-        case 0:
-            return '0';
-        case 1:
-            return '1';
-        case 2:
-            return '2';
-        case 3:
-            return '3';
-        case 4:
-            return '4';
-        case 5:
-            return '5';
-        case 6:
-            return '6';
-        case 7:
-            return '7';
-        case 8:
-            return '8';
-        case 9:
-            return '9';
-        case 10:
-            return 'A';
-        case 11:
-            return 'B';
-        case 12:
-            return 'C';
-        case 13:
-            return 'D';
-        case 14:
-            return 'E';
-        case 15:
-            return 'F';
-    }
+    if (n > 9)
+        return n - 10 + 'A';
+    return '0' + n ;
 }
