db2ba24bb0fd09de173ff93a54e8e40b08d0afa4
michelececcacci <michelececcacci1@gmail.com>
2021-10-21 16:16:26 +0200

    using standard c hex conversion

--- elaborato5/base_conversion.c
+++ elaborato5/base_conversion.c
@@ -16,8 +16,7 @@ void base_conversion_rc(unsigned int n, unsigned int b) {
         if (n >= b) {
             base_conversion_rc(n / b, b);
         }
-        unsigned char ch = n % b > 9 ? n % b  - 10 + 'A' : '0' + n % b;
-        printf("%c", ch);
+        printf("%x", n % b);
     }
 }
 
@@ -26,15 +25,10 @@ void base_conversion_it(unsigned int n, unsigned int b) {
         int num, i, digits = (int) (log(b == 2 ? n : n + 1) / log(b));
         for (i = digits - 1; i >= -1; i--) {
             num = get_n_digit(n, b, i);
-            printf("%c", convert_to_char(num));
+            printf("%x", num);
         }
         printf("\n");
     }
-}
-
-static unsigned char convert_to_char(unsigned int n) {
-    if (n > 9) return n - 10 + 'A';
-    return '0' + n;
 }
 
 static int get_n_digit(unsigned int n, unsigned int b, int digit) {
