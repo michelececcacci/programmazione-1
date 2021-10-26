079f9fbc037c74393e3fd64e6935a6569ec0c343
Michele Ceccacci <michelececcacci1@gmail.com>
2021-10-26 19:26:33 +0200

    scoring works, pads collisions still needs to be implemented

--- elaborato6/pong1.0/pong1.0/game.c
+++ elaborato6/pong1.0/pong1.0/game.c
@@ -3,23 +3,32 @@
 
 #include "game.h"
 
+/* TODO 
+decide how to implement pad collisions
+implement pads collisions 
+after everything works manually, optimize for test cases
+*/
 /* since the pad len is the same and doesn't change it is stored in game_table */
-struct game_table {
+/* not sure if this should be static */
+static struct game_table {
 	int width;
 	int height;
 	int pad_len;
 } table;
 
 /* direction is still not implemented */
-struct ball_struct {
+static struct ball_struct {
 	struct position pos;
 	struct position direction;
+	struct position initial_pos;
 } ball;	
 
-struct pad {
+static struct pad {
 	struct position pos;
 	int score;
 } pad1, pad2;
+
+static void reset_ball(void);
 
 void setup_game(int height, int width,
 	struct position ball_pos, struct position ball_dir,
@@ -33,6 +42,8 @@ void setup_game(int height, int width,
 	pad2.score = 0;
 	table.pad_len = pad_len;
 	ball.direction = ball_dir;
+	/* used to restore postion when point is scored */
+	ball.initial_pos = ball_pos;
 }
 
 void move_ball() {
@@ -41,6 +52,16 @@ void move_ball() {
 	if (ball.pos.y > table.height) {
 		ball.direction.y = -1;
 	}
+	/* if point is scored */
+	if (ball.pos.x < 0 ){
+		pad1.score += 1;
+		reset_ball();
+	}
+	if (ball.pos.x > table.width) {
+		pad2.score += 1;
+		reset_ball();
+	}
+	/* always used */
 	ball.pos.x += ball.direction.x;
 	ball.pos.y += ball.direction.y;
 }
@@ -88,6 +109,14 @@ struct position get_pad2_pos(void) {
 
 unsigned int get_pad_len(void) {
 	return table.pad_len;
+}
+
+static void reset_ball(void){
+	ball.pos = ball.initial_pos;
+}
+
+static void detect_collisons(void) {
+	ball.direction.x = - ball.direction.x;
 }
 
 #endif
