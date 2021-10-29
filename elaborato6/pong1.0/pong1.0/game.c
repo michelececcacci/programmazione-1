4348bf185b0ae63b4f3c2b72b513788387a74803
michelececcacci <michelececcacci1@gmail.com>
2021-10-29 10:10:56 +0200

    fixed pad_collisions error

--- elaborato6/pong1.0/pong1.0/game.c
+++ elaborato6/pong1.0/pong1.0/game.c
@@ -83,7 +83,7 @@ void move_ball() {
 		pad2.score++;
 		reset_ball();
 		return;
-	} else if (ball.pos.x >= table.width) {
+	} else if (ball.pos.x >= table.width - table.pad_length) {
 		pad1.score++;
 		reset_ball();
 		return;
