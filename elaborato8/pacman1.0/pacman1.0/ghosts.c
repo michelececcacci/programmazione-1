8d3cf35e501b962e525a7aa5b9743e830dc300b0
Giorgio Garofalo <giogar2705@gmail.com>
2021-11-16 20:33:28 +0100

    Move prototype

--- elaborato8/pacman1.0/pacman1.0/ghosts.c
+++ elaborato8/pacman1.0/pacman1.0/ghosts.c
@@ -1,4 +1,5 @@
 #include "global.h"
+
 #define GHOSTS_STUD
 #ifdef GHOSTS_STUD
 
@@ -14,6 +15,9 @@
 #define DOWN  1
 #define LEFT -1
 #define RIGHT 1
+
+#define MIN_4(a, b, c, d) (MIN_2(MIN_2(a, b), MIN_2(c, d)))
+#define MIN_2(a, b) (a) > (b) ? (a) : (b)
 
 typedef struct ghost {
     unsigned int id;
@@ -36,7 +40,12 @@ struct ghosts {
 static ghost *by_id(struct ghosts *G, unsigned int id);
 
 static int can_move_hor(struct arena arena, ghost *ghost, int direction);
+
 static int can_move_vert(struct arena arena, ghost *ghost, int direction);
+
+static float distance(struct position pos1, struct position pos2);
+
+static struct position closest_direction(struct arena arena, struct pacman *P, ghost *ghost);
 
 /* Create the ghosts data structure */
 struct ghosts *ghosts_setup(unsigned int num_ghosts) {
@@ -46,7 +55,7 @@ struct ghosts *ghosts_setup(unsigned int num_ghosts) {
     unsigned int i;
     for (i = 0; i < num_ghosts; i++) {
         struct position pos = {0, 0};
-        ghost ghost = { i, NORMAL, pos};
+        ghost ghost = {i, NORMAL, pos};
         ghosts->ghosts[i] = ghost;
     }
 
@@ -59,9 +68,9 @@ void ghosts_destroy(struct ghosts *G) {
 }
 
 /* Set the arena (A) matrix */
-void ghosts_set_arena(struct ghosts *G, char **A, unsigned int nrow, 
-                                                      unsigned int ncol) {
-    struct arena arena = { A, nrow, ncol };
+void ghosts_set_arena(struct ghosts *G, char **A, unsigned int nrow,
+                      unsigned int ncol) {
+    struct arena arena = {A, nrow, ncol};
     G->arena = arena;
 }
 
@@ -92,36 +101,33 @@ enum ghost_status ghosts_get_status(struct ghosts *G, unsigned int id) {
 
 /* Move the ghost id (according to its status). Returns the new position */
 struct position ghosts_move(struct ghosts *G, struct pacman *P, unsigned int id) {
-    int dir_x, dir_y, i;
     ghost *ghost = by_id(G, id);
-    /* test **/
-    if(can_move_hor(G->arena, ghost, RIGHT)) ghost->pos.i += RIGHT;  
-    if(can_move_vert(G->arena, ghost, UP)) ghost->pos.j += UP;
 
     if (ghost->status == NORMAL) {
+        struct position direction = closest_direction(G->arena, P, ghost);
+        ghost->pos.i += direction.i;
+        ghost->pos.j += direction.j;
         /* should try to find the move that brings the ghost closest to pacman */
-    }
-    else if (ghost->status == SCARED_NORMAL || ghost->status == SCARED_BLINKING) {
+    } else if (ghost->status == SCARED_NORMAL || ghost->status == SCARED_BLINKING) {
         /* same thing as before, but should bring further */
 
-    }
-    else if (ghost->status == EYES) {
+    } else if (ghost->status == EYES) {
         /* seems to work decently even though it's not pretty at all. Edge case handling not needed*/
-        char c =  G->arena.matrix[ghost->pos.j][ghost->pos.i]; 
-            switch (c) {
-        case UP_SYM:
-            ghost->pos.j += UP;
-            break;
-        case DOWN_SYM:
-            ghost->pos.j += DOWN;
-            break;
-        case LEFT_SYM:
-            ghost->pos.i += LEFT;
-            break;
-        case RIGHT_SYM:
-            ghost->pos.i += RIGHT;
-            break;
-    }
+        char c = G->arena.matrix[ghost->pos.j][ghost->pos.i];
+        switch (c) {
+            case UP_SYM:
+                ghost->pos.j += UP;
+                break;
+            case DOWN_SYM:
+                ghost->pos.j += DOWN;
+                break;
+            case LEFT_SYM:
+                ghost->pos.i += LEFT;
+                break;
+            case RIGHT_SYM:
+                ghost->pos.i += RIGHT;
+                break;
+        }
 
         /* 
         FILE *fp;
@@ -134,8 +140,8 @@ struct position ghosts_move(struct ghosts *G, struct pacman *P, unsigned int id)
 
 static ghost *by_id(struct ghosts *G, unsigned int id) {
     unsigned int i;
-    for(i = 0; i < G->num_ghosts; i++) {
-        if(G->ghosts[i].id == id) return &G->ghosts[i];
+    for (i = 0; i < G->num_ghosts; i++) {
+        if (G->ghosts[i].id == id) return &G->ghosts[i];
     }
     return NULL;
 }
@@ -148,13 +154,57 @@ static int can_move_vert(struct arena arena, ghost *ghost, int direction) {
     return arena.matrix[ghost->pos.i][ghost->pos.j + direction] == XWALL_SYM;
 }
 
+static float distance(struct position pos1, struct position pos2) {
+    float distance_x = (float) ((pos1.i - pos2.i) * (pos1.i - pos2.i));
+    float distance_y = (float) ((pos1.j - pos2.j) * (pos1.j - pos2.j));
+    return sqrtf(distance_x + distance_y);
+}
 
+static struct position closest_direction(struct arena arena, struct pacman *P, ghost *ghost) {
+    struct position pacman_pos = pacman_get_position(P);
+    struct position ghost_pos = ghost->pos;
 
-static float  distance(struct position pos1, struct position pos2){
-    float distance_x =  (pos1.i - pos2.i) * (pos1.i - pos2. i);
-    float distance_y  = (pos1.j - pos2.j) * (pos1.j - pos2.j);
-    return sqrt(distance_x + distance_y);
+    struct position new = { ghost_pos.i, ghost_pos.j };
 
+    float dis_x = 0;
+    float dis_y = 0;
+
+    int dir_x = 0;
+    int dir_y = 0;
+
+    if(can_move_hor(arena, ghost, LEFT)) {
+        new.i += LEFT;
+        dis_x = distance(pacman_pos, new);
+        dir_x = LEFT;
+        new.i -= LEFT;
+    }
+    if(can_move_hor(arena, ghost, RIGHT)) {
+        new.i += RIGHT;
+        float right_distance = distance(pacman_pos, new);
+        if(dis_x > right_distance) {
+            dis_x = right_distance;
+            dir_x = RIGHT;
+        }
+        new.i -= RIGHT;
+    }
+    if(can_move_vert(arena, ghost, UP)) {
+        new.j += UP;
+        dis_y = distance(pacman_pos, new);
+        dir_y = UP;
+        new.j -= UP;
+    }
+    if(can_move_vert(arena, ghost, DOWN)) {
+        new.j += DOWN;
+        float down_distance = distance(pacman_pos, new);
+        if(dis_y > down_distance) {
+            dis_y = down_distance;
+            dir_y = DOWN;
+        }
+        new.j += DOWN;
+    }
+
+    struct position direction = { dir_x, dir_y };
+    return direction;
 }
 
 #endif
