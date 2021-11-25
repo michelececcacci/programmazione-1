c630d12377944fa44105eb8f2b5f130db248de54
michelececcacci <michelececcacci1@gmail.com>
2021-11-25 11:48:14 +0100

    cleaner implementation of eyes, starting over again

--- elaborato8/pacman1.0/pacman1.0/ghosts.c
+++ elaborato8/pacman1.0/pacman1.0/ghosts.c
@@ -96,41 +96,16 @@ enum ghost_status ghosts_get_status(struct ghosts *G , unsigned int id){
 /* Returns 1 if the position is in the arena */
 static int is_in_arena(struct position pos, struct ghosts *G);
 
-/* Distance between 2 positions */
-static double distance(struct position pos1, struct position pos2) ;
-
-/* Direction that has the smallest possible distance from a position */
-static enum direction closest_direction(struct position old_pos, struct ghosts *G, struct pacman *P, struct ghost *ghost);
-
-/* Direction that has the biggest possible distance from a position */
-static enum direction furthest_direction(struct position old_pos, struct ghosts *G, struct pacman *P, struct ghost *ghost);
-
-
-static int can_move_dir(struct position pos, enum direction dir);
+static enum direction eyes_suggested_direction(char c);
 
 struct position ghosts_move(struct ghosts *G , struct pacman *P , unsigned int id ){
     struct ghost *g = &G->ghosts_arr[id];
     if (g->status == NORMAL){
-        g->dir = closest_direction(g->pos, G, P, g);   /* should minimize distance */
-        g->pos = new_position(g->pos, g->dir, G->nrow, G->ncol);
     }
     else if (g->status == EYES){
-        char c = G->A[g->pos.i][g->pos.j];
         struct position eyes_pos = g->pos;
-        switch (c) {
-            case UP_SYM:
-                eyes_pos.i--;
-                break;
-            case DOWN_SYM:
-                eyes_pos.i++;
-                break;
-            case LEFT_SYM:
-                eyes_pos.j--;
-                break;
-            case RIGHT_SYM:
-                eyes_pos.j++;
-                break;
-        } 
+        char c = G->A[g->pos.i][g->pos.j];
+        g->dir = eyes_suggested_direction(c);
         /* todo find a decent way to implement eyes*/
         #ifdef LOGGING
         FILE *fp;
@@ -138,22 +113,18 @@ struct position ghosts_move(struct ghosts *G , struct pacman *P , unsigned int i
         fprintf(fp ,"symbol: %c\n", c);
         fclose(fp);
         #endif
+        eyes_pos = new_position(g->pos, g->dir, G->nrow, G->ncol);
         if (is_free_other(eyes_pos, G, P)) {
-            g->dir = closest_direction(g->pos, G, P, g);   /* should maximize distance */
-            g->pos = new_position(g->pos, g->dir, G->nrow, G->ncol);
+            g->pos = eyes_pos;
         }
     }
     else {
-
-    g->pos = new_position(g->pos, g->dir, G->nrow, G->ncol);
     }
     return g->pos;
 }
 
 
-static int is_in_arena(struct position pos, struct ghosts *G){
-    return (pos.i < G->ncol + 1 && pos.j < G->nrow);
-}
+
 static struct position new_position(struct position pos, enum direction dir, unsigned int nrow, unsigned int ncol) {
 	struct position new = pos;
 	switch (dir) {
@@ -186,81 +157,21 @@ static int is_free_other(struct position pos, struct ghosts *G, struct pacman *P
     return is_free(pos, G, P);
 }
 
-static enum direction closest_direction(struct position old_pos, struct ghosts *G, struct pacman *P, struct ghost *ghost){
-        struct position pacman_pos = pacman_get_position(P);
-        struct position up_pos = old_pos, down_pos = old_pos, left_pos = old_pos, right_pos = old_pos;
-        float left_dis = 1000, right_dis = 1000, up_dis = 1000, down_dis = 1000, best_dis = 1000;
-        up_pos.i--;
-        down_pos.i++;
-        left_pos.j--;
-        right_pos.j++;
-        if (is_in_arena(up_pos, G) && is_free(up_pos, G, P)) {
-            up_dis = distance(up_pos, pacman_pos);
-        }
-        if (is_in_arena(down_pos, G) && is_free(down_pos, G, P)) {
-            down_dis = distance(down_pos, pacman_pos);
-        }
-        
-        if (is_in_arena(left_pos, G) && is_free(left_pos, G, P)) {
-            left_dis = distance(left_pos, pacman_pos);
-        }
-        if (is_in_arena(right_pos, G) && is_free(right_pos, G, P)) {
-            right_dis = distance(right_pos, pacman_pos);
-        }
-        best_dis = MIN_4(up_dis, down_dis, left_dis, right_dis);
-        #ifdef LOGGING
-        FILE *fp;
-        fp = fopen("closest_position.log", "a");
-        fprintf(fp, "up: %f, down: %f, left: %f, right: %f, best: %f\n", up_dis, down_dis, left_dis, right_dis,  best_dis);
-        fclose(fp);
-        #endif
-        if (best_dis == right_dis) return RIGHT;
-        else if (best_dis == up_dis) return UP;
-        else if (best_dis == down_dis) return DOWN;
-        else if (best_dis == left_dis) return LEFT;
-        else return UNK_DIRECTION;
-}
 
-/* not working idk why */
-enum direction furthest_direction(struct position old_pos, struct ghosts *G, struct pacman *P, struct ghost *ghost){
-    struct position pacman_pos = pacman_get_position(P);
-    struct position up_pos = old_pos, down_pos = old_pos, left_pos = old_pos, right_pos = old_pos;
-    float left_dis = 0, right_dis = 0, up_dis = 0, down_dis = 0, best_dis = 0;
-    up_pos.i--;
-    down_pos.i++;
-    left_pos.j--;
-    right_pos.j++;
-    if (is_in_arena(up_pos, G) && is_free_other(up_pos, G, P)) {
-        up_dis = distance(up_pos, pacman_pos);
-    }
-    if (is_in_arena(down_pos, G) && is_free_other(down_pos, G, P)) {
-        down_dis = distance(down_pos, pacman_pos);
-    }
-    
-    if (is_in_arena(left_pos, G) && is_free_other(left_pos, G, P)) {
-        left_dis = distance(left_pos, pacman_pos);
-    }
-    if (is_in_arena(right_pos, G) && is_free_other(right_pos, G, P)) {
-        right_dis = distance(right_pos, pacman_pos);
-    }
-    best_dis = MAX_4(up_dis, down_dis, left_dis, right_dis);
-    #ifdef LOGGING
-    FILE *fp;
-    fp = fopen("furthest_position.log", "a");
-    fprintf(fp, "up: %f, down: %f, left: %f, right: %f, best: %f\n", up_dis, down_dis, left_dis, right_dis,  best_dis);
-    fclose(fp);
-    #endif
-    if (best_dis == right_dis) return RIGHT;
-    else if (best_dis == up_dis) return UP;
-    else if (best_dis == down_dis) return DOWN;
-    else if (best_dis == left_dis) return LEFT;
-    else return UNK_DIRECTION;
-}
-
-static double distance(struct position pos1, struct position pos2) {
-    unsigned int distance_x = (pos1.i - pos2.i) * (pos1.i - pos2.i);
-    unsigned int distance_y = (pos1.j - pos2.j) * (pos1.j - pos2.j);
-    return sqrt(distance_x + distance_y);
+static enum direction eyes_suggested_direction(char c){
+    switch (c) {
+            case UP_SYM:
+            return UP;
+                break;
+            case DOWN_SYM:
+            return  DOWN;
+                break;
+            case LEFT_SYM:
+            return  LEFT;
+                break;
+            case RIGHT_SYM:
+            return RIGHT;
+        }
 }
 
 #endif
