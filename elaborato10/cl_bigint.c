0e6cf82f9648e6340e2437078bf2fb97f64de201
Michele  Ceccacci <michelececcacci1@gmail.com>
2021-12-12 17:00:09 +0100

    reverse for both

--- elaborato10/cl_bigint.c
+++ elaborato10/cl_bigint.c
@@ -5,9 +5,20 @@
 
 
 
+static void reverse_circular(struct bigint *L);
+
 bigint *mul(bigint *N1, bigint *N2) {
-	
 	return NULL;
+}
+static void reverse_circular(struct bigint *L) {
+	struct bigint *tmp = NULL, *current = L;
+	while (current !=  L) {
+		tmp = current->prev;	
+		current->prev = current->next;
+		current->next = tmp;
+		current = current->prev;
+	}
+	if (tmp != NULL) L = tmp->prev;
 }
 
 #endif
--- elaborato10/dl_bigint.c
+++ elaborato10/dl_bigint.c
@@ -1,13 +1,27 @@
+#include <stdio.h>
 #include "selector.h"
 #ifdef DL_BIGINT
 #include <stdlib.h>
 #include "dl_bigint.h"
 
+void reverse_doubly(bigint *L);
+static long int value_from_list(bigint * L);
+
 bigint *mul(bigint *N1, bigint *N2) {
-	
-	return NULL;
+	reverse_doubly(N1);
+	reverse_doubly(N2);
 }
 
+void reverse_doubly(bigint *L) {
+	struct bigint *tmp = NULL, *current = L;
+	while (current !=  NULL) {
+		tmp = current->prev;	
+		current->prev = current->next;
+		current->next = tmp;
+		current = current->prev;
+	}
+	if (tmp != NULL) L = tmp->prev;
+}
 
 
 #endif
