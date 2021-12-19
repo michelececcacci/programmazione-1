0917e2a325dca5dffb88906c334378c733000762
Giorgio Garofalo <giogar2705@gmail.com>
2021-12-19 16:21:01 +0100

    Refactor

--- elaborato10/cl_bigint.c
+++ elaborato10/cl_bigint.c
@@ -5,16 +5,27 @@
 #include <stdlib.h>
 #include "dl_bigint.h"
 
+/* Verifica se n è un numero valido */
 static int is_valid(bigint *n);
+/* Alloca ed inizializza un bigint */
 static bigint *bigint_alloc(digit x);
+/* Dealloca e rimuove un bigint */
 static int bigint_delete(bigint *N);
+/* Rimuove la testa del bigint */
 static int head_delete(bigint **N);
+/* Inserisce una cifra al bigint */
 static int bigint_insert(bigint *N, digit x);
+/* Inserisce una cifra in testa al bigint */
 static int head_insert(bigint **N, digit x);
+/* Rimuove gli zeri in eccesso */
 static void remove_zeros(bigint **N);
+/* Ottiene l'ultima cifra del bigint */
 static bigint *last(bigint *N);
+/* Effettua uno shift nel bigint */
 static int shift(bigint **n, digit x, bigint *tail);
-static void negate(bigint *n);
+/* Inverte il segno del bigint */
+static void reverse_sign(bigint *n);
+/* Ottiene il segno relativo ai bigint */
 static digit get_sign(bigint *n1, bigint *n2);
 
 bigint *mul(bigint *N1, bigint *N2) {
@@ -29,8 +40,8 @@ bigint *mul(bigint *N1, bigint *N2) {
     bigint *rem_tail_1 = last(rem);
     bigint *rem_tail_2 = last(rem);
     digit sign = get_sign(N1, N2);
-    negate(N1);
-    negate(N2);
+    reverse_sign(N1);
+    reverse_sign(N2);
 
     N1 = tail1;
     do {
@@ -123,8 +134,9 @@ static int head_insert(bigint **N, digit x) {
 
 static void remove_zeros(bigint **N) {
     if (N != NULL && *N != NULL) {
-        while ((*N)->x == 0 && *N != (*N)->next)
+        while ((*N)->x == 0 && *N != (*N)->next) {
             head_delete(N);
+        }
     }
 }
 
@@ -141,14 +153,12 @@ static int shift(bigint **n, digit x, bigint *tail) {
     return 1;
 }
 
-static void negate(bigint *n) {
+static void reverse_sign(bigint *n) {
     if (n->x < 0) n->x *= -1;
 }
 
 static digit get_sign(bigint *n1, bigint *n2) {
-    if (n1->x * n2->x < 0)
-        return -1;
-    return 1;
+    return n1->x * n2->x < 0 ? -1 : 1;
 }
 
 #endif
