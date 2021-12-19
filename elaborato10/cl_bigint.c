cccd27d8cca0199bf4ec9df15cc35eb10b2092fa
Giorgio Garofalo <giogar2705@gmail.com>
2021-12-19 15:57:02 +0100

    New circle

--- elaborato10/cl_bigint.c
+++ elaborato10/cl_bigint.c
@@ -1,44 +1,74 @@
 #include "selector.h"
+
 #ifdef CL_BIGINT
+
 #include <stdlib.h>
-#include "cl_bigint.h"
+#include "dl_bigint.h"
 
+static void negate(bigint *n);
+static bigint *last(bigint *N);
+static int exists(bigint *n);
+static int head_delete(bigint **N);
+static int bigint_delete(bigint *N);
+static bigint *bigint_alloc(digit x);
+static digit get_sign(bigint *n1, bigint *n2);
+static int head_insert(bigint **N, digit x);
+static int bigint_insert(bigint *N, digit x);
+static int shift(bigint **n, digit x, bigint *tail);
+static void remove_leading_zeros(bigint **N);
 
-#define SGN(N) (N == NULL ? 0 : ((N)->x < 0 ? -1 : 1))
+bigint *mul(bigint *N1, bigint *N2) {
+    if (N1 == NULL || N2 == NULL)
+        return NULL;
+    if (!exists(N1) || !exists(N2))
+        return NULL;
 
+    bigint *res = bigint_alloc(0);
+    bigint *tail1 = last(N1);
+    bigint *tail2 = last(N2);
+    bigint *_tailRes = last(res);
+    bigint *tailRes = last(res);
+    digit sign = get_sign(N1, N2);
+    negate(N1);
+    negate(N2);
 
-static bigint* last(bigint *L, bigint *head_node);
+    N1 = tail1;
+    do {
+        N2 = tail2;
+        res = tailRes;
+        do {
+            digit result = (digit) ((N1->x) * (N2->x));
+            res->x += result;
+            digit carry = res->x / 10;
+            res->x %= 10;
+            shift(&res, carry, _tailRes);
+            N2 = N2->prev;
+        } while (N2 != tail2);
 
-static bigint *mul_digit(bigint *N, digit x, bigint *head,bigint *tail);
+        N1 = N1->prev;
+        tailRes = tailRes->prev;
+    } while (N1 != tail1);
 
-static bigint *mul_digit_pos(bigint *N, digit x);
-
-static void add_zeros(bigint *N, unsigned int n, bigint *tail); 
-
-static int tail_insert(bigint **N, digit x, bigint *tail);
-
-static bigint *sum_pos(bigint *N1, bigint *N2);
-
-static void delete(bigint **N);
-
-static void negate(bigint *N);
-
-static bigint* bigint_alloc(digit x) {
-	bigint* tmp = (bigint*)malloc(sizeof(bigint));
-
-	if (tmp != NULL) {
-		tmp->x = x;
-		tmp->next = NULL;
-		tmp->prev = NULL;
-	}
-	return tmp;
+    remove_leading_zeros(&res);
+    res->x *= sign;
+    return res;
 }
 
-static int bigint_delete(bigint* N) {
-	if (N == NULL) {
-		return 1;
-	}
-    else {
+static bigint *bigint_alloc(digit x) {
+    bigint *tmp = (bigint *) malloc(sizeof(bigint));
+
+    if (tmp != NULL) {
+        tmp->x = x;
+        tmp->next = tmp;
+        tmp->prev = tmp;
+    }
+    return tmp;
+}
+
+static int bigint_delete(bigint *N) {
+    if (N == NULL) {
+        return 1;
+    } else {
         bigint *prv = N->prev, *nxt = N->next;
         nxt->prev = prv;
         prv->next = nxt;
@@ -47,38 +77,31 @@ static int bigint_delete(bigint* N) {
     }
 }
 
-static int bigint_insert(bigint *N, digit x){
-    if (N == NULL){
+static int bigint_insert(bigint *N, digit x) {
+    if (N == NULL) {
         return 1;
-    }
-    else {
-        bigint *tmp = bigint_alloc(x), *nxt = N->next, *prv = N;
+    } else {
+        bigint *tmp = bigint_alloc(x), *next = N->next, *prev = N;
         if (tmp != NULL) {
-            tmp->prev = prv;
-            tmp->next = nxt;
-            prv->next = tmp;
-            nxt->prev = tmp;
+            tmp->prev = prev;
+            tmp->next = next;
+            prev->next = tmp;
+            next->prev = tmp;
         }
         return tmp == NULL;
     }
 }
 
-static int head_insert(bigint** N, digit x) {
-    if (N == NULL)
-        return 1;
-    else if (*N == NULL) 
-        return ((*N = bigint_alloc(x)) == NULL);
-    else if(bigint_insert((*N)->prev, x) == 1)
-        return 1;
-    else 
-        return ((*N = (*N)->prev) == NULL);
+static int head_insert(bigint **N, digit x) {
+    if (*N == NULL) return (*N = bigint_alloc(x)) == NULL;
+    else if (bigint_insert((*N)->prev, x) == 1) return 1;
+    else return (*N = (*N)->prev) == NULL;
 }
 
-static int head_delete(bigint** N) {
-	if (N == NULL || *N == NULL) {
-		return 1;
-	}
-    else if (*N == (*N)->next) {
+static int head_delete(bigint **N) {
+    if (*N == NULL) {
+        return 1;
+    } else if (*N == (*N)->next) {
         free(*N);
         *N = NULL;
         return 0;
@@ -88,117 +111,45 @@ static int head_delete(bigint** N) {
     }
 }
 
-static void remove_leading_zeros(bigint** N) {
-	if (N != NULL && *N != NULL) {
-        while ((*N)->x == 0 && *N !=(*N)->next)
+static void remove_leading_zeros(bigint **N) {
+    if (N != NULL && *N != NULL) {
+        while ((*N)->x == 0 && *N != (*N)->next)
             head_delete(N);
     }
 }
 
-bigint *mul(bigint *N1, bigint *N2) {
-    int sgn1 = SGN(N1), sgn2 = SGN(N2), n = 0;
-    bigint  *n1_head = N1, *n2_head = N2, *tail2 = last(N2, n2_head), *tail1 = last(N1, N1);
-    if (!sgn1 || !sgn2) return NULL;
-    bigint *tmp = last(N2, tail2), *N = bigint_alloc(0);
-    while (tmp != n2_head){ 
-        bigint *a, *b;
-        a = mul_digit(N1, tmp->x, n1_head, tail1);
-        add_zeros(a, n++, tail1);
-        b = sum_pos(N, a);
-        delete(&N);
-        delete(&a);
-        N = b;
-        tmp = tmp->prev;
-    } 
-    if (sgn1 != sgn2) negate(N);
-    return N;
+static bigint *last(bigint *N) {
+    return N->prev;
 }
 
-static bigint* last(bigint *L, bigint *head_node) {
-    while (L->next != head_node && L->next){
-        L = L->next;
+static int shift(bigint **n, digit x, bigint *tail) {
+    if ((*n)->prev == tail) {
+        return head_insert(n, x);
     }
-    return L;
+    *n = (*n)->prev;
+    (*n)->x += x;
+    return 1;
 }
 
-static bigint *mul_digit(bigint *N, digit x, bigint *head,bigint *tail) {
-    if (!N || x > 9 || x < -9)  {
-        return NULL;
-    }
-    else if (!x) {
-        return bigint_alloc(0);
-    }
-    else {
-        N->x = abs(N->x);
-        bigint* X = mul_digit_pos(N, abs(x));
-        return X;
-    } 
+static void negate(bigint *n) {
+    if (n->x < 0) n->x *= -1;
 }
 
-static bigint *mul_digit_pos(bigint *N, digit x) {
-    bigint *X = NULL;
-    int val = 0, car = 0; 
-    bigint *head = N;
-    N = N->prev;
-    while ((N != NULL) || car != 0){
-        val = (N ? N->x : 0) * x + car;
-        car = val / 10;
-        val = val % 10;
-        head_insert(&X, val);
-        N = N != NULL && N != head ? N->prev: NULL;
-    }
-    return X;
-}
-
-static void add_zeros(bigint *N, unsigned int n, bigint *tail) {
-    if (N && N->x) {
-        int i;
-        for (i = 0; i < n; i++)
-            tail_insert(&N, 0, tail);
-    }
-}
-
-static int tail_insert(bigint **N, digit x, bigint *tail) {
-    if (!N)
+static digit get_sign(bigint *n1, bigint *n2) {
+    if (n1->x * n2->x < 0)
+        return -1;
+    else
         return 1;
-    else if (!(*N)) {
-        return head_insert(N, x);
-    }
-    else {
-        bigint *tmp = tail;
-        tmp->next = bigint_alloc(x);
-        if (tmp->next)
-            tmp->next->prev = tmp;
-        return tmp->next != NULL;
-    }
 }
 
-static bigint *sum_pos(bigint *N1, bigint *N2) {
-    bigint *N = NULL;
-        bigint *head1 = N1,  *head2 = N2;
-        N1 = N1->prev;
-        N2 = N2->prev;
-        while (N1 || N2 || car) {
-            val = (N1 ? N1->x : 0) + (N2 ? N2->x : 0) + car;
-            car = val / 10;
-            val = val % 10;
-            head_insert(&N, val);
-            N1 = N1 != NULL && N1 != head1 ? N1->prev: NULL;
-            N2 = N2 != NULL && N2 != head2 ? N2->prev: NULL;
+static int exists(bigint *n) {
+    if (n->x > 9 || n->x < -9) return 0;
+    bigint *next;
+    for (next = n->next; next != n; next = next->next) {
+        if (next->x > 9 || next->x < 0)
+            return 0;
     }
-    return N;
+    return 1;
 }
 
-static void delete(bigint **N){
-if (*N && N) {
-        while ((*N)->next) {
-            head_delete(N);
-        }
-    }
-}
-
-static void negate(bigint *N){
-    if (N) 
-        N->x *= -1;
-}
 #endif
